f91bb08f784a3647b4564c514a2e0e9afeddbcf9
ruicun <2953wrq@163.com>
2017-07-15 22:09:34 +0800

    fix

--- src/modules/TrailDetail/index.js
+++ src/modules/TrailDetail/index.js
@@ -14,7 +14,7 @@ class TrailDetail extends Component {
           <div className="info">
             <div className="name">张三</div>
             <div className="phone">13262664172</div>
-            <div>浙江临安市的一个小村庄</div>
+            <div className="address">浙江临安市的一个小村庄</div>
           </div>
         </div>
         <div className="trail-info">
@@ -30,6 +30,34 @@ class TrailDetail extends Component {
           <div className="detail-content">这是一块服务详情的内容占位。这是一块服务详情的内容占位。这是一块服务详情的内容占位。这是一块服务详情的内容占位。这是一块服务详情的内容占位。</div>
           <div className="info-block">
             <span className="name">评论：</span>
+          </div>
+          <div className="comment">
+            <div className="avatar">
+              <img src="http://img.kan300.com/userup/1312/16125JVW1.jpg" alt="" />
+            </div>
+            <div className="content">
+              <div className="author-info">
+                <span>张三</span>
+                <span>06-28 16:30</span>
+              </div>
+              <div className="text">
+                这歌老板的阿斯顿利卡得利卡岁的卢卡斯了打开拉萨打开；拉斯达克；拉萨。
+              </div>
+            </div>
+          </div>
+          <div className="comment">
+            <div className="avatar">
+              <img src="http://img.kan300.com/userup/1312/16125JVW1.jpg" alt="" />
+            </div>
+            <div className="content">
+              <div className="author-info">
+                <span>张三</span>
+                <span>06-28 16:30</span>
+              </div>
+              <div className="text">
+                这歌老板的阿斯顿利卡得利卡岁的卢卡斯了打开拉萨打开；拉斯达克；拉萨。
+              </div>
+            </div>
           </div>
         </div>
       </div>);
