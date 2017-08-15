9349baf1bbc7a9b9d9b5d62a3247f14e5347bc7c
suyuan <suyuan1573@gmail.com>
2017-08-15 22:22:53 +0800

    add home

--- src/App.js
+++ src/App.js
@@ -16,12 +16,15 @@ import './styles/index.less';
 const App = () => (
   <div>
     <Route exact path="/user-hw/" component={Home} />
-    <Route exact path="/user-hw/list" component={List} />
-    <Route exact path="/user-hw/profile/edit" component={ProfileEditor} />
-    <Route exact path="/user-hw/trail/list" component={TrailList} />
-    <Route exact path="/user-hw/trail/detail" component={TrailDetail} />
+    <Route exact path="/user-hw/profile/edit/:user_id" component={ProfileEditor} />
+
     <Route exact path="/user-hw/activity/list/:admin_id" component={ActivityList} />
     <Route exact path="/user-hw/activity/join/:activity_id" component={ActivityJoin} />
+
+    <Route exact path="/user-hw/trail/list" component={List} />
+    <Route exact path="/user-hw/trail/detail" component={TrailDetail} />
+
+    <Route exact path="/user-hw/list" component={TrailList} />
   </div>
 );
 
--- src/modules/Home/index.js
+++ src/modules/Home/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
+import {Link} from 'react-router-dom';
+
+import './index.less';
+
 const Home = () => (
-  <div />
+    <div>
+      <ul className='home-ul'>
+        <li><Link to="/user-hw/profile/edit/1">填写保单</Link></li>
+        
+        
+        <li><Link to="/user-hw/activity/list/1">活动列表[done]</Link></li>
+        <li><Link to="/user-hw/activity/join/2">参加活动[done]</Link></li>
+        
+        <li><Link to="/user-hw/trail/list">地接列表</Link></li>
+        <li><Link to="/user-hw/trail/detail">地接详情</Link></li>
+
+        <li><Link to="/user-hw/list">不知道什么页面</Link></li>
+        
+      </ul>
+    </div>
 );
 
 export default Home;
